3f6c281b47d4f6cf4bd4ead3bbb47cd7054a1f7b
yutao <yutao_forever@hotmail.com>
2020-07-09 10:56:54 +0800

    no message

---
+++ UnityEditorUIExpansion/BUITextEditor.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityEditor.UI
+{
+    [CustomEditor(typeof(BUIText), true)]
+    [CanEditMultipleObjects]
+    /// <summary>
+    ///   Custom Editor for the Text Component.
+    ///   Extend this class to write a custom editor for an Text-derived component.
+    /// </summary>
+    public class BlitzTextEditor : GraphicEditor
+    {
+        SerializedProperty m_Text;
+        SerializedProperty m_FontData;
+        //SerializedProperty m_OutLine;
+        //SerializedProperty m_OutLineColor;
+        //SerializedProperty m_OutLineOffsetX;
+        //SerializedProperty m_OutLineOffsetY;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            m_Text = serializedObject.FindProperty("m_Text");
+            m_FontData = serializedObject.FindProperty("m_FontData");
+            //m_OutLine = serializedObject.FindProperty("m_OutLine");
+            //m_OutLineColor = serializedObject.FindProperty("m_OutLineColor");
+            //m_OutLineOffsetX = serializedObject.FindProperty("m_OutLineOffsetX");
+            //m_OutLineOffsetY = serializedObject.FindProperty("m_OutLineOffsetY");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            EditorGUILayout.PropertyField(m_Text);
+            EditorGUILayout.PropertyField(m_FontData);
+            //EditorGUILayout.PropertyField(m_OutLine);
+            //EditorGUILayout.PropertyField(m_OutLineColor);
+            //EditorGUILayout.PropertyField(m_OutLineOffsetX);
+            //EditorGUILayout.PropertyField(m_OutLineOffsetY);
+            AppearanceControlsGUI();
+            RaycastControlsGUI();
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
--- UnityEngineUIExpansion/BUIText.cs
+++ UnityEngineUIExpansion/BUIText.cs
@@ -1,10 +1,102 @@
 using System;
+using System.Collections.Generic;
 using UnityEngineUIExpansion;
 namespace UnityEngine.UI
 {
     [AddComponentMenu("UI/BUI Text", 10)]
     public class BUIText : Text
     {
+        //[SerializeField] private bool m_OutLine = true;
+        //[SerializeField] private Color m_OutLineColor = Color.black;
+        //[SerializeField] private float m_OutLineOffsetX = 1f;
+        //[SerializeField] private float m_OutLineOffsetY = -1f;
+
+        //UIVertex[] m_TempVerts = new UIVertex[4];
+        //protected override void OnPopulateMesh(VertexHelper toFill)
+        //{
+        //    if (font == null)
+        //        return;
+
+        //    // We don't care if we the font Texture changes while we are doing our Update.
+        //    // The end result of cachedTextGenerator will be valid for this instance.
+        //    // Otherwise we can get issues like Case 619238.
+        //    m_DisableFontTextureRebuiltCallback = true;
+
+        //    Vector2 extents = rectTransform.rect.size;
+
+        //    var settings = GetGenerationSettings(extents);
+        //    cachedTextGenerator.PopulateWithErrors(text, settings, gameObject);
+
+        //    // Apply the offset to the vertices
+        //    IList<UIVertex> verts = cachedTextGenerator.verts;
+        //    float unitsPerPixel = 1 / pixelsPerUnit;
+        //    //Last 4 verts are always a new line... (\n)
+        //    int vertCount = verts.Count - 4;
+
+        //    // We have no verts to process just return (case 1037923)
+        //    if (vertCount <= 0)
+        //    {
+        //        toFill.Clear();
+        //        return;
+        //    }
+
+        //    Vector2 roundingOffset = new Vector2(verts[0].position.x, verts[0].position.y) * unitsPerPixel;
+        //    roundingOffset = PixelAdjustPoint(roundingOffset) - roundingOffset;
+        //    toFill.Clear();
+        //    if (roundingOffset != Vector2.zero)
+        //    {
+        //        for (int i = 0; i < vertCount; ++i)
+        //        {
+        //            int tempVertsIndex = i & 3;
+        //            m_TempVerts[tempVertsIndex] = verts[i];
+        //            m_TempVerts[tempVertsIndex].position *= unitsPerPixel;
+        //            m_TempVerts[tempVertsIndex].position.x += roundingOffset.x;
+        //            m_TempVerts[tempVertsIndex].position.y += roundingOffset.y;
+        //            if (tempVertsIndex == 3)
+        //                toFill.AddUIVertexQuad(m_TempVerts);
+        //        }
+        //    }
+        //    else
+        //    {
+        //        for (int i = 0; i < vertCount; ++i)
+        //        {
+        //            int tempVertsIndex = i & 3;
+        //            m_TempVerts[tempVertsIndex] = verts[i];
+        //            m_TempVerts[tempVertsIndex].position *= unitsPerPixel;
+        //            if (m_OutLine && tempVertsIndex == 3)
+        //            {
+        //                ApplyShadowZeroAlloc(ref m_TempVerts, m_OutLineColor, m_OutLineOffsetX, m_OutLineOffsetY, toFill);
+        //                ApplyShadowZeroAlloc(ref m_TempVerts, m_OutLineColor, m_OutLineOffsetX, -m_OutLineOffsetY, toFill);
+        //                ApplyShadowZeroAlloc(ref m_TempVerts, m_OutLineColor, -m_OutLineOffsetX, m_OutLineOffsetY, toFill);
+        //                ApplyShadowZeroAlloc(ref m_TempVerts, m_OutLineColor, -m_OutLineOffsetX, -m_OutLineOffsetY, toFill);
+        //                toFill.AddUIVertexQuad(m_TempVerts);
+        //            }
+        //        }
+        //    }
+
+        //    m_DisableFontTextureRebuiltCallback = false;
+        //}
+        //private void ApplyShadowZeroAlloc(ref UIVertex[] rVertex, Color rEffectColor, float rEffectDistanceX, float rEffectDistanceY, VertexHelper rHelper)
+        //{
+        //    for (int i = 0; i < rVertex.Length; i++)
+        //    {
+        //        Vector3 rPosition = rVertex[i].position;
+        //        rPosition.x += rEffectDistanceX;
+        //        rPosition.y += rEffectDistanceY;
+        //        rVertex[i].position = rPosition;
+        //        rVertex[i].color = rEffectColor;
+        //    }
+        //    rHelper.AddUIVertexQuad(rVertex);
+        //    for (int i = 0; i < rVertex.Length; i++)
+        //    {
+        //        Vector3 rPosition = rVertex[i].position;
+        //        rPosition.x -= rEffectDistanceX;
+        //        rPosition.y -= rEffectDistanceY;
+        //        rVertex[i].color = color;
+        //        rVertex[i].position = rPosition;
+        //    }
+        //}
+
         Action _onChangeCallback;
         public override string text
         {
